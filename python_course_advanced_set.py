c26559e8867631feead61e96cf794d97e12b2335
Gany Chidambaram <ganapathy.c@gmail.com>
2019-11-10 13:06:48 -0800

    first set of files

---
+++ python_course_advanced_set.py
@@ -0,0 +1,14 @@
+set1 = {22, 33, 44}
+set2 = {44, 55, 66}
+
+diff1 = set1.difference(set2)
+print(diff1)
+
+diff2 = set2.difference(set1)
+print(diff2)
+
+uni = set1.union(set2)
+print(uni)
+
+inter = set1.intersection(set2)
+print(inter)
---
+++ python_course_list_tuple_set.py
@@ -0,0 +1,13 @@
+list = ["Bob", "Smith", "Pence"]
+tuple = ("Bob", "Smith", "Pence")
+set = {"Bob", "Smith", "Pence"}
+
+print(list[0])
+list[0] = "Ganapathy"
+list.append("tak-tak-nu")
+print(list)
+
+
+print(set)
+set.remove("Pence")
+print(set)
