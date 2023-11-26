0d1d4df25a0264b1a877049356fe328855951615
Ganapathy Chidambaram <ganapathychidambaram@Ganapathys-MacBook-Air.local>
2023-11-26 21:45:51 +0530

    added algorithm for partition - WB

---
+++ partition_get_all.py
@@ -0,0 +1,8 @@
+"""
+Given a list of distinct positive numbers,
+list all ways in which this list can be split into 3 sub-sets
+such that they all have the same sum
+
+1. Create a recursive method to get all subsets that sum up to the target
+2. Get all combinations of 3 subsets that don't overlap with one another in terms of members
+"""
