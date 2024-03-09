3b4a641617f867810475c69a1c907fd2f4117d34
Gany Chidambaram <ganapathy.c@gmail.com>
2024-03-08 19:27:29 -0700

    Update get_rsi_stocks.py
    removed rolling means; used percentages in gain and loss

--- misc/get_rsi_stocks.py
+++ misc/get_rsi_stocks.py
@@ -2,12 +2,13 @@ import json
 import yfinance as yf
 import numpy as np
 
+
+
 """
+
 Curl for file download:
 
-curl -X POST https://www.nyse.com/api/quotes/filter -H "Content-Type: application/json" -d '{"instrumentType":"EQUITY","pageNumber":1,"sortColumn":"NORMALIZED_TICKER","sortOrder":"ASC","maxResultsPerPage":7059,"filterToken":""}'
-
-
+curl -X POST https://www.nyse.com/api/quotes/filter -H "Content-Type: application/json" -d '{"instrumentType":"EQUITY","pageNumber":1,"sortColumn":"NORMALIZED_TICKER","sortOrder":"ASC","maxResultsPerPage":7059,"filterToken":""}' > completeNyse.txt
 """
 
 # Calculate RSI
@@ -18,18 +19,20 @@ def calculate_RSI(ticker, periods=14):
     gain = delta.where(delta > 0, 0)
     loss = -delta.where(delta < 0, 0)
 
-    avg_gain = gain.rolling(window=periods).mean()
-    avg_loss = loss.rolling(window=periods).mean()
+    gain_percentage = gain / stock_data['Close'].shift(1) * 100
+    loss_percentage = loss / stock_data['Close'].shift(1) * 100
+
+    avg_gain = gain_percentage.mean()
+    avg_loss = loss_percentage.mean()
 
     rs = avg_gain / avg_loss
     rsi = 100 - (100 / (1 + rs))
     print("RSI = ", rsi)
-    print(rsi.shape)    
-    return rsi[-1] if len(rsi) > 0 else np.nan
+    return rsi
 
 tickers = []
 
-with open('/Users/gchidambara/Desktop/completeNyse.txt', 'r') as file:
+with open('/Users/gchidambara/completeNyse.txt', 'r') as file:
     rows = json.load(file)
 
 for item in rows:
